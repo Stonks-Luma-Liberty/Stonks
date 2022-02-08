602a3eb6746ef7be9a0d06ff162431ce137ad2b2
Christian Pérez Villanueva <perez.villanueva.christian34@gmail.com>
2022-02-08 14:30:09 -0400

    Included error handling for price command

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from discord import Bot, Embed, ButtonStyle, AllowedMentions
 from discord.commands import Option, permissions
 from discord.ext.pages import Paginator, PaginatorButton
 from discord.ui import View
+from requests.exceptions import RequestException
 from tortoise import Tortoise
 
 from api.coingecko import CoinGecko
@@ -38,71 +39,88 @@ async def price(
     """
     logger.info("Price command executed")
     pages = []
-    coin_ids = await get_coin_ids(symbol=symbol.upper())
 
-    for ids in coin_ids:
-        coin_stats = await get_coin_stats(coin_id=ids)
-        percent_change_24h = coin_stats["percent_change_24h"]
-        percent_change_7d = coin_stats["percent_change_7d"]
-        percent_change_30d = coin_stats["percent_change_30d"]
+    try:
+        coin_ids = await get_coin_ids(symbol=symbol.upper())
 
-        embed_message = Embed(
-            title=f"{coin_stats['name']} ({coin_stats['symbol']})",
-            url=coin_stats["website"],
-            colour=0xC5E519,
-        )
-        embed_message.add_field(
-            name="Explorers 🔗",
-            value=", ".join(coin_stats["explorers"]),
-            inline=False,
-        )
-        embed_message.add_field(name="Price 💸", value=coin_stats["price"], inline=False)
-        embed_message.add_field(
-            name="Market Cap Rank 🥇",
-            value=coin_stats["market_cap_rank"],
-            inline=False,
-        )
-        embed_message.add_field(
-            name="Market Cap 🏦", value=coin_stats["market_cap"], inline=False
-        )
-        embed_message.add_field(
-            name="Volume 💰", value=coin_stats["volume"], inline=False
-        )
-        embed_message.add_field(
-            name="24H Change 📈" if percent_change_24h > 0 else "24H Change 📉",
-            value=f"{percent_change_24h}%",
-            inline=False,
-        )
-        embed_message.add_field(
-            name="7D Change 📈" if percent_change_7d > 0 else "7D Change 📉",
-            value=f"{percent_change_7d}%",
-            inline=True,
-        )
-        embed_message.add_field(
-            name="30D Change 📈" if percent_change_30d > 0 else "30D Change 📉",
-            value=f"{percent_change_30d}%",
-            inline=True,
-        )
+        for ids in coin_ids:
+            coin_stats = await get_coin_stats(coin_id=ids)
+            percent_change_24h = coin_stats["percent_change_24h"]
+            percent_change_7d = coin_stats["percent_change_7d"]
+            percent_change_30d = coin_stats["percent_change_30d"]
 
-        if "percent_change_ath" in coin_stats:
-            percent_change_ath = coin_stats["percent_change_ath"]
+            embed_message = Embed(
+                title=f"{coin_stats['name']} ({coin_stats['symbol']})",
+                url=coin_stats["website"],
+                colour=0xC5E519,
+            )
             embed_message.add_field(
-                name="ATH Change 📈" if percent_change_ath > 0 else "ATH Change 📉",
-                value=f"{percent_change_ath}%",
+                name="Explorers 🔗",
+                value=", ".join(coin_stats["explorers"]),
+                inline=False,
+            )
+            embed_message.add_field(name="Price 💸", value=coin_stats["price"], inline=False)
+            embed_message.add_field(
+                name="Market Cap Rank 🥇",
+                value=coin_stats["market_cap_rank"],
+                inline=False,
+            )
+            embed_message.add_field(
+                name="Market Cap 🏦", value=coin_stats["market_cap"], inline=False
+            )
+            embed_message.add_field(
+                name="Volume 💰", value=coin_stats["volume"], inline=False
+            )
+            embed_message.add_field(
+                name="24H Change 📈" if percent_change_24h > 0 else "24H Change 📉",
+                value=f"{percent_change_24h}%",
+                inline=False,
+            )
+            embed_message.add_field(
+                name="7D Change 📈" if percent_change_7d > 0 else "7D Change 📉",
+                value=f"{percent_change_7d}%",
                 inline=True,
             )
-        pages.append(embed_message)
-    paginator = Paginator(pages=pages, use_default_buttons=False)
-    paginator.add_button(
-        PaginatorButton(button_type="prev", label="", style=ButtonStyle.red, emoji="⬅")
-    )
-    paginator.add_button(
-        PaginatorButton("page_indicator", style=ButtonStyle.gray, disabled=True)
-    )
-    paginator.add_button(
-        PaginatorButton(button_type="next", style=ButtonStyle.green, emoji="➡")
-    )
-    await paginator.respond(ctx.interaction)
+            embed_message.add_field(
+                name="30D Change 📈" if percent_change_30d > 0 else "30D Change 📉",
+                value=f"{percent_change_30d}%",
+                inline=True,
+            )
+
+            if "percent_change_ath" in coin_stats:
+                percent_change_ath = coin_stats["percent_change_ath"]
+                embed_message.add_field(
+                    name="ATH Change 📈" if percent_change_ath > 0 else "ATH Change 📉",
+                    value=f"{percent_change_ath}%",
+                    inline=True,
+                )
+            pages.append(embed_message)
+        paginator = Paginator(pages=pages, use_default_buttons=False)
+        paginator.add_button(
+            PaginatorButton(button_type="prev", label="", style=ButtonStyle.red, emoji="⬅")
+        )
+        paginator.add_button(
+            PaginatorButton("page_indicator", style=ButtonStyle.gray, disabled=True)
+        )
+        paginator.add_button(
+            PaginatorButton(button_type="next", style=ButtonStyle.green, emoji="➡")
+        )
+        await paginator.respond(ctx.interaction)
+    except TypeError as e:
+        logger.error(e)
+        embed_message = Embed(
+            title=f"Data for ({symbol}) is not available",
+            colour=0xC5E519,
+        )
+        await ctx.respond(embed=embed_message)
+    except RequestException as e:
+        logger.error(e)
+        embed_message = Embed(
+            title=f"Unable to get data for ({symbol}) at this time",
+            colour=0xC5E519,
+        )
+        await ctx.respond(embed=embed_message)
+
 
 
 @bot.slash_command()
